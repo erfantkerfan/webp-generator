c5f7c0b41d5aa9d917da605cb8cf1db252332e45
erfantkerfan <erfantkerfan@yahoo.com>
2020-09-06 20:37:06 +0430

    fix crontab

--- webp.py
+++ webp.py
@@ -3,7 +3,7 @@
 """
 ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
 put this in crontab as root user
-* 0 * * * python3 -u /home/alaa/webp/webp.py >> /home/alaa/webp/log.txt 2>&1
+0 0 * * * python3 -u /home/alaa/webp/webp.py >> /home/alaa/webp/log.txt 2>&1
 SHELL=/bin/bash
 ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
 """
